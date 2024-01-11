953b050593a648ad05898794b8d0b8b230e967d3
kastastin <kostya242421@gmail.com>
2024-01-11 16:11:58 +0200

    hotel-booking gallery section (adaptive)

--- HotelBooking/js/script.js
+++ HotelBooking/js/script.js
@@ -6,7 +6,7 @@ menuBtnEl.addEventListener("click", () => navbarEl.classList.toggle("active"));
 document.addEventListener("scroll", () => navbarEl.classList.remove("active"));
 
 // <-- Swiper home -->
-const swiper = new Swiper(".home-slider", {
+const swiperHome = new Swiper(".home-slider", {
   loop: true,
   effect: "coverflow",
   grabCursor: true,
@@ -17,6 +17,30 @@ const swiper = new Swiper(".home-slider", {
     depth: 100,
     modifier: 1,
     slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// <-- Swiper gallery -->
+const swiperGallery = new Swiper(".gallery-slider", {
+  loop: true,
+  effect: "coverflow",
+  slidesPerView: "auto",
+  centeredSlides: true,
+  grabCursor: true,
+  speed: 800,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
   },
   navigation: {
     nextEl: ".swiper-button-next",
