39523d9ac3061f6af8c752769ef9bc9251284a82
kastastin <kostya242421@gmail.com>
2024-01-13 01:40:14 +0200

    bakery header, nav (adaptive)

--- Bakery/assets/js/main.js
+++ Bakery/assets/js/main.js
@@ -1,17 +1,32 @@
-/*=============== SHOW MENU ===============*/
+// <-- show menu -->
+const navMenuEl = document.querySelector("#nav-menu");
+const navToggleEl = document.querySelector("#nav-toggle");
+const navCloseEl = document.querySelector("#nav-close");
 
+navToggleEl.addEventListener("click", () =>
+  navMenuEl.classList.add("show-menu")
+);
 
-/*=============== REMOVE MENU MOBILE ===============*/
+navCloseEl.addEventListener("click", () =>
+  navMenuEl.classList.remove("show-menu")
+);
 
+// <-- remove menu mobile -->
+const navLinksElems = document.querySelectorAll(".nav__link");
+
+navLinksElems.forEach((linkEl) =>
+  linkEl.addEventListener("click", linkHandler)
+);
+
+function linkHandler() {
+  const navMenuEl = document.querySelector("#nav-menu");
+  navMenuEl.classList.remove("show-menu");
+}
 
 /*=============== ADD BLUR HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
