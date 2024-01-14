bfd98579fc18d312fda4edcd4bef3d7190ff33b5
kastastin <kostya242421@gmail.com>
2024-01-14 20:49:35 +0200

    headphones header, home, about (mobile-first)

--- Headphones/assets/js/main.js
+++ Headphones/assets/js/main.js
@@ -1,19 +1,50 @@
-/*=============== SHOW MENU ===============*/
+// <-- show menu -->
+const navMenuEl = document.querySelector("#nav-menu");
+const navToggleEl = document.querySelector("#nav-toggle");
+const navCloseEl = document.querySelector("#nav-close");
 
+navToggleEl.addEventListener("click", () =>
+  navMenuEl.classList.add("show-menu")
+);
 
-/*=============== REMOVE MENU MOBILE ===============*/
+navCloseEl.addEventListener("click", () =>
+  navMenuEl.classList.remove("show-menu")
+);
 
+document.addEventListener("click", (e) => {
+  if (
+    !e.target.classList.contains("ri-menu-line") &&
+    !e.target.classList.contains("show-menu")
+  )
+    navMenuEl.classList.remove("show-menu");
+});
 
-/*=============== ADD BLUR HEADER ===============*/
+// <-- remove menu mobile -->
+const navLinksElems = document.querySelectorAll(".nav__link");
 
+navLinksElems.forEach((linkEl) =>
+  linkEl.addEventListener("click", linkHandler)
+);
 
-/*=============== SWIPER FAVORITES ===============*/ 
+function linkHandler() {
+  const navMenuEl = document.querySelector("#nav-menu");
+  navMenuEl.classList.remove("show-menu");
+}
 
+// <-- add blur header -->
+window.addEventListener("scroll", headerHandler);
 
-/*=============== SHOW SCROLL UP ===============*/ 
+function headerHandler() {
+  const headerEl = document.querySelector("#header");
+  this.scrollY >= 50
+    ? headerEl.classList.add("blur-header")
+    : headerEl.classList.remove("blur-header");
+}
 
+/*=============== SWIPER FAVORITES ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
