2aa8e32d76dffcd985beabec26d4d5827c5e5300
kastastin <kostya242421@gmail.com>
2024-01-17 00:19:08 +0200

    Aipro how, benefit, who sections (375px)

--- Aipro/assets/js/main.js
+++ Aipro/assets/js/main.js
@@ -47,3 +47,66 @@ links.forEach((linkEl) => {
     });
   };
 });
+
+// <-- add blur header -->
+window.addEventListener("scroll", headerHandler);
+
+function headerHandler() {
+  const headerEl = document.querySelector("#header");
+  this.scrollY >= 50
+    ? headerEl.classList.add("blur-header")
+    : headerEl.classList.remove("blur-header");
+}
+
+// <-- show scroll up tooltip -->
+window.addEventListener("scroll", scrollUpHandler);
+
+function scrollUpHandler() {
+  const scrollUpEl = document.querySelector("#scroll-up");
+  this.scrollY >= 350
+    ? scrollUpEl.classList.add("show-scrollup")
+    : scrollUpEl.classList.remove("show-scrollup");
+}
+
+// <-- scroll sections active links -->
+const sectionsElems = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler() {
+  const scrollDown = window.scrollY;
+
+  sectionsElems.forEach((sectionEl) => {
+    const sectionHeight = sectionEl.offsetHeight;
+    const sectionTop = sectionEl.offsetTop - 58;
+    const sectionId = sectionEl.getAttribute("id");
+    const sectionClass = document.querySelector(
+      `.nav__menu a[href*=${sectionId}]`
+    );
+
+    scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight
+      ? sectionClass.classList.add("active-link")
+      : sectionClass.classList.remove("active-link");
+  });
+}
+
+// <-- swiper benefits -->
+const swiperBenefits = new Swiper(".benefit__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 1.5,
+  centeredSlides: true,
+  spaceBetween: 30,
+  speed: 400,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 3,
+  //   },
+  // },
+});
